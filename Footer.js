3f4e4d7467c31e1516d7fcbd542064ceb2f578af
Tanya Yadav <121024100+Tanyaayadav@users.noreply.github.com>
2025-03-29 00:12:27 +0530

    Create Footer.js

---
+++ Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white text-center p-4 mt-8">
+      © 2025 Fitness Club. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
