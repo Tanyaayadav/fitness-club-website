8312da85ed29166b2d1b8b575f0b3471ca45370b
Tanya Yadav <121024100+Tanyaayadav@users.noreply.github.com>
2025-03-29 00:40:39 +0530

    Delete Footer.js

--- Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-900 text-white text-center p-4 mt-8">
-      © 2025 Fitness Club. All rights reserved.
-    </footer>
-  );
-};
-
-export default Footer;
